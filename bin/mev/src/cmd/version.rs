5166f875d589d5295dd27c97d878949c3a295f69
lean-apple <78718413+lean-apple@users.noreply.github.com>
2024-10-25 14:02:40 +0700

    docs: improve version information docs

--- bin/mev/src/cmd/version.rs
+++ bin/mev/src/cmd/version.rs
@@ -1,9 +1,6 @@
 use const_format::{concatcp, str_index};
 use std::fmt;
 
-/// The human readable name of the client
-pub const NAME_CLIENT: &str = "mev";
-
 /// The latest version from Cargo.toml
 pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -29,13 +26,41 @@ pub const GIT_COMMIT_HASH: &str = env!("VERGEN_RUSTC_COMMIT_HASH", "unknown");
 pub const GIT_COMMIT_SHORT: &str = str_index!(GIT_COMMIT_HASH, ..8);
 
 /// The build features.
-pub const VERGEN_CARGO_FEATURES: &str = env!("VERGEN_CARGO_FEATURES");
+pub const VERGEN_CARGO_FEATURES: &str = env!("VERGEN_CARGO_FEATURES", "none");
 
-/// The short version information
+/// The short version information for mev.
+///
+/// - The latest version from Cargo.toml
+/// - The short SHA of the latest commit
+///
+/// # Example
+///
+/// ```text
+/// mev v0.3.0 (f6e511ee)
+/// ```
 pub const SHORT_VERSION: &str =
-    concatcp!(NAME_CLIENT, " v", CARGO_PKG_VERSION, VERSION_SUFFIX, " (", GIT_COMMIT_SHORT, ")");
+    concatcp!("v", CARGO_PKG_VERSION, VERSION_SUFFIX, " (", GIT_COMMIT_SHORT, ")");
 
-/// The long version information
+/// The long version information for mev.
+///
+/// - The latest version from Cargo.toml
+/// - The full SHA of the latest commit
+/// - The build timestamp
+/// - The target triple
+/// - The rustc version
+/// - The build features
+///
+/// # Example:
+///
+/// ```text
+/// Version:     0.3.0
+/// Commit:      f6e511eec7342f59a25f7c0534f1dbea00d01b14
+/// Built:       2024-10-25T05:46:13.173948000Z
+/// Target:      aarch64-apple-darwin
+/// Rustc:       1.82.0
+/// Features:    boost,build,default,mev_boost_rs,mev_build_rs,mev_relay_rs,relay,reth
+/// ```
+
 pub const LONG_VERSION: &str = concatcp!(
     "Version:     ",
     CARGO_PKG_VERSION,
@@ -95,7 +120,6 @@ mod tests {
     #[test]
     fn test_short_version_information() {
         let short_version_info = Version::short_version();
-        assert!(short_version_info.starts_with(NAME_CLIENT));
         assert!(short_version_info.contains(CARGO_PKG_VERSION));
         assert!(short_version_info.contains("("));
         assert!(short_version_info.contains(")"));
